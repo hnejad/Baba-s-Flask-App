a7b5988bbb2e64762a067977307a15ae9b087960
codeMagic122 <30890276+codeMagic122@users.noreply.github.com>
2020-08-30 10:50:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-from queryData import queryData
+from queryData import queryData, getHeaders
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     data = queryData()
-    return render_template("index.html",data=data)
+    headers = getHeaders()
+    return render_template("index.html",data=data, headers=headers)
 
 if __name__ == "__main__":
     app.run(debug=1)
