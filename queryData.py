99087b961e0b79fb0021bf5a3a2e4dcb42845145
codeMagic122 <30890276+codeMagic122@users.noreply.github.com>
2020-08-30 10:50:46 -0700

    Update queryData.py

--- queryData.py
+++ queryData.py
@@ -1,3 +1,7 @@
 def queryData():
     data = [['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth'],['first','second','third','fourth']]
     return data
+
+def getHeaders():
+    data = ['Column 1', 'Column 2', 'Column 3', 'Column 4']
+    return data
